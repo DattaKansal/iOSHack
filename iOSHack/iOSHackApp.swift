f92c207cf28dd6560948abaa2a4f32ae7108b84c
DattaKansal <dtkansal@gmail.com>
2024-10-18 19:10:27 -0400

    Initial Commit

---
+++ iOSHack/iOSHackApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSHackApp.swift
+//  iOSHack
+//
+//  Created by Datta Kansal on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct iOSHackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
