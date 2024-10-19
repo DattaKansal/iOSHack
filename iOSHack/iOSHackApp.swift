f658bdda4329d926917a60c6ca16e3149a0519a3
Akshat-Shenoi <ashenoi3@gatech.edu>
2024-10-18 21:19:28 -0400

    WelcomeView

--- iOSHack/iOSHackApp.swift
+++ iOSHack/iOSHackApp.swift
@@ -7,6 +7,14 @@
 
 import SwiftUI
 
+extension Color {
+    static let primaryBackground = Color("primaryBackground") // From asset catalog
+    static let secondaryBackground = Color("secondaryBackground")
+    static let primary = Color(red: 172/255, green: 216/255, blue: 170/255)
+    static let secondary = Color(red: 147/255, green: 133/255, blue: 129/255)
+    static let bg = Color(red: 51/255, green: 51/255, blue: 51/255)
+}
+
 @main
 struct iOSHackApp: App {
     var body: some Scene {
